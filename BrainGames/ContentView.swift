6449cc4351f910f741758eaa8cfcad027f76c6fc
Ryan Keys <r.keys1998@gmail.com>
2020-12-06 19:28:30 -0800

    Initial Commit

---
+++ BrainGames/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrainGames
+//
+//  Created by Ryan Keys on 12/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
